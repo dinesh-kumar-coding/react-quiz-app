4763dd31eb2c01adb7b3edbaf91dfaae9c52a1f1
Dinesh Kumar <dineshpucchal@gmail.com>
2026-06-22 14:24:32 +0530

    Initial commit - quiz app

---
+++ src/App.jsx
@@ -0,0 +1,112 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [isAnswered, setIsAnswered] = useState(false);
+  const [score, setScore] = useState(0);
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const questions = [
+    {
+      question: "What is the capital of France?",
+      options: ["London", "Paris", "Berlin", "Madrid"],
+      answer: "Paris",
+    },
+    {
+      question: "Which language runs in a web browser?",
+      options: ["Python", "C++", "JavaScript", "Java"],
+      answer: "JavaScript",
+    },
+    {
+      question: "What does CSS stand for?",
+      options: [
+        "Computer Style Sheets",
+        "Cascading Style Sheets",
+        "Creative Style System",
+        "Colorful Style Sheets",
+      ],
+      answer: "Cascading Style Sheets",
+    },
+  ];
+
+  const isFinished = currentQuestion >= questions.length;
+  const current = questions[currentQuestion];
+
+  function checkAnswer(option) {
+    if (option === current?.answer) {
+      setScore((prev) => prev + 1);
+    }
+    setSelectedOption(option);
+    setIsAnswered(true);
+  }
+
+  function handleNext() {
+    setCurrentQuestion((prev) => prev + 1);
+    setIsAnswered(false);
+    setSelectedOption(null);
+  }
+
+  function handleRestart(){
+    setScore(0);
+    setIsAnswered(false);
+    setCurrentQuestion(0);
+    setSelectedOption(null);
+  }
+
+  function getButtonStyle(option){
+    if(!isAnswered){
+      return "";
+    }
+    if(option === current.answer){
+      return "correct";
+    }
+    if(option === selectedOption){
+      return "wrong"
+    }
+    return "";
+  }
+
+  if(isFinished){
+    return(
+      <>
+        <h2>Quiz Completed!</h2>
+        <p>You scored {score} out of {questions.length}</p>
+        <button onClick={handleRestart}>Restart</button>
+      </>
+    )
+  }
+
+  return (
+    <>
+      <h2>Question: {current?.question}</h2>
+      {current?.options.map((option, index) => {
+        return (
+          <button
+            className={getButtonStyle(option)}
+            disabled={isAnswered}
+            key={index}
+            onClick={() =>{
+              checkAnswer(option);
+            }}
+          >
+            {option}
+          </button>
+        );
+      })}
+
+      {isAnswered && currentQuestion < questions.length && (
+        <button
+          onClick={() => {
+            handleNext();
+          }}
+        >
+          {currentQuestion === questions.length - 1? "Finish": "Next"}
+        </button>
+      )}
+    </>
+  );
+}
+
+export default App;
